d742c51ca00c80dfad56d12e58c05f1b8eb723c2
ranveersingh281120 <ranveersingh281120@gmail.com>
2026-03-07 17:14:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let student = {
+	name:"ranveer",
+	age:25,
+	city:"hyd"
+};
+function getKeys(student) {
+	return Object.name(student);
+	
+}
+console.log(getKeys(student))
